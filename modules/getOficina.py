b6acb09d82953fd56b3a826006c0f9707fe069d1
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-03-06 06:55:48 -0500

    Terminado de momento

---
+++ modules/getOficina.py
@@ -0,0 +1,22 @@
+import storage.oficina as of
+
+def getAllCodigoCiudad():
+    codigoCiudad = []
+    for val in of.oficina:
+        codigoCiudad.append({
+            "codigo": val.get("codigo_oficina"),
+            "ciudad": val.get("ciudad")
+        })
+    return codigoCiudad
+
+def getAllCiudadTelefono(pais)
+    ciudadTelefono = []
+    for val in of.oficina:
+        if (val.get("pais") == pais):
+            ciudadTelefono.append({
+                "ciudad": val.get("ciudad"),
+                "telefono": val.get("telefono")
+                "oficinas" : val.get("cpodigo_oficina"),
+                "pais" : val.get("pais") 
+            })
+    return ciudadTelefono
