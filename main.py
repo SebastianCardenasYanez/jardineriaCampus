17931db7162c24d3f4a18a1c03cffca1c21b1526
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-03-07 13:49:17 -0500

    ejercicio 14 :0

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import modules.getOficina as oficina
 import modules.getEmpleados as empleado
 import modules.getPedido as pedidos
 import modules.getPago as pago
-print(tabulate(pedidos.getAllPedidosEntregados(), tablefmt = 'rounded_grid'))
+print(tabulate(pago.getAllFormaDePago(), tablefmt = 'rounded_grid'))
 
--- modules/getPago.py
+++ modules/getPago.py
@@ -9,3 +9,15 @@ def getAllCodigoClienteAñoPago() :
             codigoPago.add(val.get('codigo_cliente'))
     return codigoPago
         
+#ejercicio 13
+def getAllFormaDePago() :
+    formaPago = []
+    #sorted ()reverse = true
+    for val in pag.pago:
+        año = val.get("fecha_pago")
+        forma = val.get("forma_pago")
+        if año.startswith("2008") and forma == "PayPal":
+            formaPago.append(val)
+    return formaPago
+        
+#ejercicio 14
