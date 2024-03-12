90481a7e2b02b05a510c43f73d6f346dfb9384f6
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-03-12 07:34:27 -0500

    producto terminado

--- modules/getProducto.py
+++ modules/getProducto.py
@@ -3,19 +3,35 @@ import storage.producto as pro
 def getAllStockPriceGama (gama, stock):
     condiciones =[]
     for val in pro.producto:
-        if (val.get("gama") == gama and val.get("precio_venta") >= stock):
-            condiciones.append({
-                "gama" : val.get("gama"),
-                "precio de venta" : val.get("precio_venta") 
-            })
+        if (val.get("gama") == gama and val.get("cantidad_en_stock") >= stock):
+            condiciones.append(val)
+            
+            # condiciones.append({
+            #     "gama" : val.get("gama"),
+            #     "precio de venta" : val.get("precio_venta") 
+            # })
             
             #     "gama" : val.get("gama"),
             #     "cantidad_en_stock" : val.get("canditad_en_stock") 
             
     def price(val):
-        if val.get("precio_venta") != None:
-            condiciones.sort(key=price)
-            
         return val.get("precio_venta")
+    condiciones.sort(key=price, reverse=True)
+    for i, val in enumerate(condiciones) : 
+        condiciones[i] = {
+            "codigo": val.get("codigo_producto"),
+            "venta": val.get("precio_venta"),
+            "nombre": val.get("nombre"),
+            "gama": val.get("gama"),
+            "dimensiones" : val.get("dimensiones"),
+            "proveedor": val.get("proveedor"), 
+            "descripcion": f'(val.get("descripcion")[15])...' if condiciones[1].get("descripcion") else val.get("descripcion"),
+            "stock": val.get("cantidad_en_stock"),
+            "base" : val.get("precio_proveedor")
+        }
+        # if val.get("precio_venta") != None:
+        #     condiciones.sort(key=price)
+            
+        # return val.get("precio_venta")
     return condiciones
 
