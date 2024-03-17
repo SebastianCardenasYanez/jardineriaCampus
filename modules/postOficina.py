3daf92c7dfda698303a5df05b663228f4b9b1648
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-03-16 19:18:17 -0500

    post oficina hecho paaaaaaaaaa

--- modules/postOficina.py
+++ modules/postOficina.py
@@ -13,17 +13,63 @@ def postOficina():
             if(not oficina.get("codigo_oficina")):
                 codigo = input("Ingrese el codigo de la oficina: ")
                 if (re.match(r'^[A-Z]{3}-[A-Z]{2}$', codigo)is not None):
-                    oficina['codigo_Oficina'] = codigo
+                    oficina['codigo_oficina'] = codigo
                 else : 
                     raise Exception("El codigo no comple con el estandar establecido ")
-                
             if (not oficina.get("ciudad")):
                 ciudad = input("Ingrese la ciudad de la ofcina: ")
                 if (re.match(r'^[A-Z][a-z]*\s*)+$', ciudad)is not None):
                     oficina["nombre"] = ciudad
                     break
                 else:
-                        raise Exception("El nombre no comple con el estandar establecido")
+                        raise Exception("La ciudad no comple con el estandar establecido")
+            if (not oficina.get("pais")):
+                pais = input("Ingrese el pais de la ofcina: ")
+                if (re.match(r'^[A-Z][a-z])+$', pais)is not None):
+                    oficina["pais"] = pais
+                    break
+                else:
+                    raise Exception("El pais no comple con el estandar establecido")
+            if (not oficina.get("region")):
+                region = input("Ingrese la region de la ofcina: ")
+                if (re.match(r'^[A-Z][a-z])+$', region)is not None):
+                    oficina["region"] = region
+                    break
+                else:
+                    raise Exception("La region no comple con el estandar establecido")
+
+            if(not oficina.get("codigo_postal")):
+                codpos = input("Ingrese el codigo postal de la oficina: ")
+                if (re.match(r'^[A-Z]{3}-[A-Z]{2}$', codpos)is not None):
+                    oficina['codigo_postal'] = codpos
+                else : 
+                    raise Exception("El codigo no comple con el estandar establecido ")
+
+            if (not oficina.get("telefono")):
+                tel = input("Ingrese la id de la transaccion: ")
+                if (re.match(r'^[+0-9]{2}\s[0-9]{2}\s[0-9]{7}$', tel)is not None):
+                        oficina["telefono"] = tel
+                        break
+                else:
+                    raise Exception("El id no comple con el estandar establecido")
+
+            if (not oficina.get("linea_direccion1")):
+                direc1 = input("Ingrese la id de la transaccion: ")
+                if (re.match(r'^[A-Z][a-z][0-9]*\s*)$', direc1)is not None):
+                        oficina["telefono"] = direc1
+                        break
+                else:
+                    raise Exception("El id no comple con el estandar establecido")
+
+            if (not oficina.get("linea_direccion2")):
+                direc2 = input("Ingrese la id de la transaccion: ")
+                if (re.match(r'^[A-Z][a-z][0-9]*\s*)$', direc2)is not None):
+                        oficina["telefono"] = direc2
+                        break
+                else:
+                    raise Exception("El id no comple con el estandar establecido")
+
+            
         except Exception as error: 
             print(error)
         print(oficina)
